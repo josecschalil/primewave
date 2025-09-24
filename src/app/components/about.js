bb6748fe6d11b28fb14bddaf3e8ae245fcf97dab
jose <josecschalil@gmail.com>
2025-09-24 05:56:01 +0530

    apos fix 6

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -36,7 +36,7 @@ const AboutMeSection = () => {
   const quotes = [
     "Code is poetry written in logic.",
     "Every bug is a learning opportunity in disguise.",
-    "Clean code is not written by following a set of rules. It's written by someone who cares.",
+    "Clean code is not written by following a set of rules. It&apos;s written by someone who cares.",
     "The best error message is the one that never shows up.",
     "Simplicity is the ultimate sophistication in programming.",
   ];
